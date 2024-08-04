87a956a127debf5e9a9b4e074cf0838e71457613
dhairyaaa15 <dhairyaitm123@gmail.com>
2024-08-04 22:38:42 +0530

    reload

--- app.py
+++ app.py
@@ -57,5 +57,5 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
