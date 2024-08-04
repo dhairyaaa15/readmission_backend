923d6ff41ee85fc3dc25ace498f6fe79fc904f42
dhairyaaa15 <dhairyaitm123@gmail.com>
2024-08-04 22:29:52 +0530

    prod

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Load the trained model (replace 'model.sav' with your actual model filename)
 model = joblib.load('logisticmodel.sav')
@@ -14,10 +16,10 @@ def predict():
     try:
         # Get JSON data from the request
         data = request.json
-        
+
         # Convert JSON data to DataFrame
         df = pd.DataFrame([data])
-        
+
         # Ensure the columns match the features used during training
         expected_features = ['age', 'time_in_hospital', 'num_procedures', 'num_medications', 
                              'number_outpatient_log1p', 'number_emergency_log1p', 
@@ -39,14 +41,14 @@ def predict():
                              'level1_diag1_1.0', 'level1_diag1_2.0', 'level1_diag1_3.0', 
                              'level1_diag1_4.0', 'level1_diag1_5.0', 'level1_diag1_6.0', 
                              'level1_diag1_7.0', 'level1_diag1_8.0']
-        
+
         # Ensure the input data has all required features
         df = df.reindex(columns=expected_features, fill_value=0)
 
         # Make prediction
         prediction = model.predict(df)
         prediction_proba = model.predict_proba(df)
-        
+
         # Return the prediction as JSON
         return jsonify({
             'prediction': int(prediction[0]), 
